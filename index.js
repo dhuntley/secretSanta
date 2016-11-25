44828c8877ff9257d344098d66aa90a3542a1da9
Huntley, Daniel <Daniel_Huntley@intuit.com>
2016-11-24 22:41:19 -0700

    Update payload

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ var app = express();
 app.set("port", (process.env.PORT || 5000));
 
 app.get("/", function(request, response) {
-  const params = request.query;
-  console.log(params.codename);
-  response.setHeader("content-type", "text/javascript");
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.send(JSON.stringify(params));
+	const params = request.query;
+	console.log(params.codename);
+	response.setHeader("content-type", "text/javascript");
+	response.setHeader("Access-Control-Allow-Origin", "*");
+	response.send(JSON.stringify({
+		realName: "Daniel",
+		targetName: "Jenny"
+	}));
 });
 
 app.listen(app.get("port"), function() {
-  console.log("Node app is running on port", app.get("port"));
+	console.log("Node app is running on port", app.get("port"));
 });
