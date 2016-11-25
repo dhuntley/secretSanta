47b260e1a5ecb385e2a8a6c6f04882c9e14e7aaf
Huntley, Daniel <Daniel_Huntley@intuit.com>
2016-11-24 22:09:49 -0700

    Add CORS header

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get("/", function(request, response) {
   const params = request.query;
   console.log(params.name);
+  response.setHeader("content-type", "text/javascript");
+  response.setHeader("Access-Control-Allow-Origin", "*");
   response.send(JSON.stringify(params));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
